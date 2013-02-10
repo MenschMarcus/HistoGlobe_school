c72654c8e7a953994132a9bff97edea75cca21ee
Simon Schneegans <code@simonschneegans.de>
2013-02-10 11:15:17 +0100

    added info page

---
+++ index.php
@@ -0,0 +1,182 @@
+<?php
+    session_start();
+    
+    // define language whitelist
+    $allowedLangs = array('en', 'de');
+    
+    if(isset($_GET['lang']) && in_array($_GET['lang'], $allowedLangs)) {
+        $_SESSION['lang'] = $_GET['lang'];
+    }
+    if(!isset($_SESSION['lang'])) {
+        $_SESSION['lang'] = 'de'; // default value
+    }
+    include('locale/' . $_SESSION['lang'] . '.php'); // include lang file
+    
+    function locale($phrase) {
+        global $lang;
+        
+        if(array_key_exists($phrase, $lang)) {
+            echo $lang[$phrase];
+        } else {
+            echo $phrase;
+        }
+    } 
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de-de" lang="de-de" dir="ltr">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <title>HistoGlobe</title>
+        
+        <link rel="icon" type="image/png" href="img/favicon.png">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        
+        <script type="text/javascript" src="js/jquery-1.9.0.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+	            $(".smooth").click(function(event){		
+		            event.preventDefault();
+		            $('html,body').animate({scrollTop:$($(this).attr('href')).offset().top}, 500);
+	            });
+	            
+	            $('#demo-link').tooltip();
+            });
+        </script>
+
+    </head>
+    <body data-spy="scroll" data-target="#mainNavigation" data-offset="20">
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand active smooth" href="#home">
+                        <img src="img/logo_small.svg" alt="HistoGlobe">
+                    </a> 
+                    <div class="nav-collapse collapse" id="mainNavigation">
+                        <ul class="nav">
+                            <li class=""><a class="smooth" href="#home"><i class="<?php locale("iconHome")?>"></i> <?php locale("buttonHome")?></a></li>
+                            <li class=""><a class="smooth" href="#summary"><i class="<?php locale("iconDetails")?>"></i> <?php locale("buttonDetails")?></a></li>
+                            <li class=""><a class="smooth" href="#about"><i class="<?php locale("iconAbout")?>"></i> <?php locale("buttonAbout")?></a></li>
+                            <li class=""><a class="smooth" href="#contact"><i class="<?php locale("iconContact")?>"></i> <?php locale("buttonContact")?></a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-collapse collapse">
+                        <ul class="nav pull-right">
+                            <li class="dropdown" id="fat-menu">
+                                <a data-toggle="dropdown" class="dropdown-toggle" role="button" id="language-drop" href="#"><i class="icon-comment-alt"></i> Language <b class="caret"></b></a>
+                                <ul aria-labelledby="language-drop" role="menu" class="dropdown-menu">
+                                    <li class=""><a href="?lang=de">Deutsch</a></li>
+                                    <li class=""><a href="?lang=en">English</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" id="home">
+            <div class="hero-unit">
+                <center>
+                    <img src="img/logo_big.svg" alt="logo">
+                    <!--<a href="#" id="demo-link" class="btn" data-placement="bottom" data-original-title="Warnung: Der Globus benötigt einen sehr aktuellen Browser."><small>3D-Globus anzeigen!</small></a>-->
+                </center>
+            </div>
+            
+            <div class="info-box">
+                <div class="row">
+                    <p><i class="icon-warning-sign pull-left" style="font-size:300%"></i>  <?php locale("not_ready")?></p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="span4">
+                    <div class="gradient-down summary">
+                        <h3><i class="<?php locale("icon_1")?>"></i> <?php locale("feature_1a")?></h3>
+                        <p><?php locale("summary_1")?> <br>
+                        <a class="smooth" href="#summary"> <?php locale("read_on")?></a></p>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="gradient-down summary">
+                        <h3><i class="<?php locale("icon_2")?>"></i> <?php locale("feature_2a")?></h3>
+                        <p><?php locale("summary_2")?> <br>
+                        <a class="smooth" href="#summary2"> <?php locale("read_on")?></a></p>
+                    </div>
+                </div>
+                <div class="span4">
+                    <div class="gradient-down summary">
+                        <h3><i class="<?php locale("icon_3")?>"></i> <?php locale("feature_3a")?></h3>
+                        <p><?php locale("summary_3")?> <br>
+                        <a class="smooth" href="#summary3"> <?php locale("read_on")?></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" id="summary">
+            <div class="details gradient-up">
+                <div class="row">
+                    <i class="<?php locale("icon_1")?> pull-left icon-feature"></i> 
+                    <h2><?php locale("feature_1a")?> <span class="muted"><?php locale("feature_1b")?></span></h2> 
+                    <?php locale("explanation_1")?>
+                </div>
+
+                <hr id="summary2">
+
+                <div class="row">
+                    <i class="<?php locale("icon_2")?> pull-right icon-feature"></i> 
+                    <h2><?php locale("feature_2a")?> <span class="muted"><?php locale("feature_2b")?></span></h2> 
+                    <?php locale("explanation_2")?>
+                </div>
+
+                <hr id="summary3">
+
+                <div class="row">
+                    <i class="<?php locale("icon_3")?> pull-left icon-feature"></i> 
+                    <h2><?php locale("feature_3a")?> <span class="muted"><?php locale("feature_3b")?></span></h2> 
+                    <?php locale("explanation_3")?>
+                </div>
+            </div>
+        </div>
+
+        <div class="container" id="about"> 
+            <div class="details gradient-down">
+                <div class="row">
+                    <h4><i class="<?php locale("iconAbout")?>"></i> <?php locale("buttonAbout")?></h4>
+                    <?php locale("about")?>
+                </div> 
+            </div>
+        </div> 
+            
+        <div class="container" id="contact"> 
+            <div class="row" >
+                <div class="span6">
+                    <div class="details muted">
+                        <h4><i class="<?php locale("iconContact")?>"></i> <?php locale("buttonContact")?></h4>
+                        <small><?php locale("contact")?></small>
+                    </div> 
+                </div> 
+                <div class="span6">
+                    <div class="details muted">
+                        <h4><i class="<?php locale("iconImpressum")?>"></i> <?php locale("buttonImpressum")?></h4>
+                        <small><?php locale("impressum")?></small>
+                    </div> 
+                </div> 
+            </div> 
+
+        </div>
+    </body>
+</html>
---
+++ locale/de.php
@@ -0,0 +1,59 @@
+<?php
+    $lang = array(
+        'buttonHome' => 'Start',
+        'buttonDetails' => 'Projektziele',
+        'buttonAbout' => 'Hintergrund',
+        'buttonContact' => 'Kontakt',
+        'buttonImpressum' => 'Impressum',
+        
+        'not_ready' => '<span class="hg">HistoGlobe</span> wird eine Plattform, die den Verlauf der Geschichte auf einer Weltkarte visualisiert. Der nächste Prototyp wird Ende Juni 2013 erwartet.<br/>
+        Das Projekt wird durch die Heinz Nixdorf Stiftung und die Stiftung der Deutschen Wirtschaft im Rahmen von Herausforderung Unternehmertum 2013 gefördert.',
+        
+        'iconHome' => 'icon-home',
+        'iconDetails' => 'icon-ok',
+        'iconAbout' => 'icon-info-sign',
+        'iconContact' => 'icon-envelope-alt',
+        'iconImpressum' => 'icon-comments',
+        
+        'icon_1' => 'icon-lightbulb',
+        'icon_2' => 'icon-cogs',
+        'icon_3' => 'icon-globe',
+        
+        'feature_1a' => 'Informativ',
+        'feature_2a' => 'Dynamisch',
+        'feature_3a' => 'Frei',
+        
+        'feature_1b' => 'und lehrreich',   
+        'feature_2b' => 'und interaktiv',
+        'feature_3b' => 'für jeden',
+        
+        'summary_1' => '<span class="hg">HistoGlobe</span> hilft, Geschichte und ihre Zusammenhänge besser zu verstehen. Dazu werden historische Ereignisse und Verläufe ansprechend auf einer Weltkarte visualisiert und mit Quellen verknüpft.',
+        'summary_2' => '<span class="hg">HistoGlobe</span> verwendet modernste Web-Technologien, um Geschichte nutzbar und interaktiv darzustellen. Die Plattform bietet eine Zeitleiste und ein Vielzahl von Optionen, um die dargestellten Informationen nach eigenen Wünschen zu filtern.',
+        'summary_3' => 'Nutzer von <span class="hg">HistoGlobe</span> können historische Daten zusammentragen, verknüpfen und diskutieren. Als Web-Plattform ist es weltweit offen zugänglich und ermöglicht so den Aufbau einer lebhaften Nutzergemeinschaft.',
+        
+        'explanation_1' => '<p><span class="hg">HistoGlobe</span> wird ein gemeinnütziges, offenes Portal für Schüler und Studenten, Lehrer und Dozenten, sowie alle historisch Interessierten. Es ist ein innovativer Zugang zur Geschichte: ein neues Medium als Ergänzung zum Lehrbuch, um historische Zusammenhänge greifbarer und verständlicher zu machen. Die Verknüpfung einer Fülle historischer Ereignisse und deren visueller Aufarbeitung in Raum und Zeit machen <span class="hg">HistoGlobe</span> zu einem einzigartigen Lehrmaterial. Es hat das Potential, die klassische Wandkarte im Geschichtsunterricht zu ersetzen.</p>
+        <p>Eine Verwendung ist in Schulen, an Universitäten, in Museen und als Nachschlagewerk zum Lernen möglich. <span class="hg">HistoGlobe</span> hilft, mit Animationen und Grafiken komplexe historische Entwicklungen zu verstehen.</p>',
+        
+        'explanation_2' => '<p><span class="hg">HistoGlobe</span> wird die zeitgemäßen Möglichkeiten des Internets nutzen, um eine dynamische und interaktive Darstellung der Geschichte zu schaffen. Es wird möglich sein, Ereignisse auf einer zweidimensionalen Karte oder auf einem dreidimensionalen Globus zu betrachten. Dabei kann der angezeigte Teil der Welt frei gewählt und der aktuelle Zeitpunkt auf einem Zeitstrahl eingestellt werden. Darüber hinaus kann nach der Art und Quelle von Ereignissen gefiltert werden. Zu jedem dargestellten Ereignis liefert <span class="hg">HistoGlobe</span> auf einen Blick die wichtigsten Daten. Weiterführende Informationen zu Kriegen, Entdeckungsreisen, Grenzverschiebungen und vielen anderen Ereignistypen sind nur einen Mausklick entfernt.</p>',
+        
+        'explanation_3' => '<p>Der Zugang zu <span class="hg">HistoGlobe</span> wird so barrierefrei wie möglich gestaltet. Jeder Mensch, der Zugang zum Internet hat, wird historische Informationen abrufen können, da das Portal plattformunabhängig in allen gängigen Internet-Browsern funktionieren wird. Von registrierten Nutzern können die gespeicherten Daten bearbeitet und neue Ereignisse erstellt werden. Ein Bewertungssystem und die Redaktion von <span class="hg">HistoGlobe</span> garantieren dabei eine hohe Zuverlässigkeit der dargestellten Informationen.</p><p>Geschichte ist ein kontroverses Themengebiet, deshalb lädt <span class="hg">HistoGlobe</span> zum offenen Dialog und zum Diskutieren ein. Für jedes Ereignis können diverse Quellen gegeneinander abgewogen und unterschiedliche Darstellungen vorgeschlagen werden. Dadurch wird <span class="hg">HistoGlobe</span> zu einer umfassenden Sammlung verschiedener Blickrichtungen auf die Geschichte unserer Welt.</p>',
+
+        'about' => '<img src="img/ut.jpg" class="img-right pull-right" alt="UT-prämiert"><p><span class="hg">HistoGlobe</span> ist ein gemeinnütziges Projekt, das sich für eine gute historische Bildung einsetzt und moderne Web-Technologien dafür nutzen will, Geschichte zugänglich und greifbar zu machen. Der Inhalt von <span class="hg">HistoGlobe</span> soll von einer weltweiten Gemeinschaft Geschichtsbegeisterter geschaffen werden. </p>
+        <p>Sechs Studierende unterschiedlicher Fachrichtungen aus Deutschland entwickeln das Projekt gemeinsam. Es wird von <a href="http://www.herausforderung-unternehmertum.de" target="_blank">Herausforderung Unternehmertum 2013</a>, einer Initiative der Heinz Nixdorf Stiftung und der Stiftung der Deutschen Wirtschaft gefördert.</p>
+        <img src="img/ict.jpg" class="img-left pull-left" alt="ICT-prämiert">
+      <p>Die Idee entstand im Jahr 2008 an einem Gymnasium im Süden Brandenburgs. Motiviert wurde sie durch das Problem der Geschichtslehrer, für jeden neuen Zeitabschnitt eine neue Weltkarte auf den Kartenständer zu hängen, obwohl alle Karten dieselbe Erde, nur mit unterschiedlichen Grenzen, Namen, Symbolen und Zeitstempeln zeigen. Daraus entstand die Vision von einem Lehrmaterial, welches dieselbe Karte nutzt und historische Ereignisse und wechselnde Grenzverläufe auf dieser darstellt.</p>
+      <p>Seit 2010 wird das Projekt <span class="hg">HistoGlobe</span> an unterschiedlichen Standpunkten kontinuierlich konzipiert und umgesetzt. <a href="http://www.histoglobe.com/old" target="_blank">Der aktuelle Prototyp</a> wurde im Rahmen eines Projektwettbewerbs an der Åbo Akademi in Turku (Finnland) im Winter 2011/2012 entwickelt. Die damalige Projektgruppe, bestehend aus sechs internationalen Studierenden, gewann mit diesem Prototypen zwei Publikumspreise und den Preis für die beste Präsentation beim Projektwettbewerb <a href="https://research.it.abo.fi/news-and-events/stories/ict-showroom-is-an-annual-event-for-student-projects" target="_blank">ICT Showroom 2012</a>.</p>',
+        
+        'contact' => 'Wenn Sie Fragen zur Entwicklung des Projekts, Ideen, Anregungen oder Kritik haben, schreiben Sie einfach eine Nachricht an:
+        <a href="mailto:info@histoglobe.com">info@histoglobe.com</a>',
+
+        'impressum' => '<p>
+        Marcus Kossatz -  Brunnenstraße 3 - 99423 Weimar <br />
+        Telefon: 03563 / 389 119 <br />
+        E-Mail: <a href="mailto:marcus.kossatz@histoglobe.com">marcus.kossatz@histoglobe.com</a><br />
+        &copy; 2013 Alle Rechte vorbehalten<br >
+        Der Betreiber übernimmt keine Verantwortung für die Inhalte von Websites, die über Verlinkungen dieses Portals erreicht werden. </p>',
+        
+        'read_on' => 'Weitere Informationen <i class="icon-double-angle-down"></i>',
+    );
+?>
