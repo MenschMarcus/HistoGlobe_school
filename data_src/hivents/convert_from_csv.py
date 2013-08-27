475491c2cb14a5a6ce18321684ac78dd11a1aaa8
Felix Lauer <felix.lauer@uni-weimar.de>
2013-08-27 14:48:22 +0200

    added eu-data and script to convert them into formats needed by HistoGlobe

---
+++ data_src/hivents/convert_from_csv.py
@@ -0,0 +1,94 @@
+###############################################################
+#This script converts csv files containing hivent data into
+#formats needed by HistoGlobe: one hivent_collection.json file
+#referencing all hivents and one [HIVENT_ID].htm file for each
+#hivent.
+#
+#Data within the csv file has to be formatted as follows:
+#
+#ID|name|description|date|locName|long|lat
+#
+###############################################################
+
+import sys, os
+import csv
+
+def main():
+  csv_file_path = ""
+  target_path = ""
+
+  if len(sys.argv) < 3:
+    print "A csv file and a target path have to be specified!"
+    print "Usage: convert_from_csv.py PATH_TO_CSV TARGET_PATH"
+    return -1
+
+  csv_file_path = sys.argv[1]
+  target_path = sys.argv[2]
+
+  assets_path =  target_path + "/hivent_assets/"
+  html_path = assets_path + "html/"
+
+  if not os.path.exists(html_path):
+    os.makedirs(html_path)
+
+  json_target = open(target_path + "/hivent_collection.json", "w")
+  json_target.write("[\n")
+
+
+  with open(csv_file_path, 'rb') as csvfile:
+    rows = list(csv.reader(csvfile, delimiter='|', quotechar='\"'))
+    row_count = len(rows)
+    for row in rows:
+      if row != rows[0]:
+        hivent_id = row[0]
+        hivent_name = row[1]
+        hivent_description = row[2]
+        hivent_date = row[3]
+        hivent_location = row[4]
+        hivent_long = row[5]
+        hivent_lat = row[6]
+
+        #create html
+        html_name = hivent_id + ".htm"
+        html_target = open(html_path + html_name, "w")
+        html_target.write('<!DOCTYPE html>\n'+
+                          '<head>\n' +
+                          '\t<link rel="stylesheet" type="text/css" href="style/hivent_info_content.min.css">\n'+
+                          '\t<title></title>\n' +
+                          '</head>\n')
+        html_target.write('<body>\n' +
+                           '\t<h3>' + hivent_location + ', ' +
+                           hivent_date + '</h3>\n' +
+                           '\t<p>\n\t\t' +
+                           hivent_description +
+                           '\n\t</p>\n' +
+                           '</body>'
+                         )
+        html_target.close()
+        #create json
+
+        json_target.write('\t{\n')
+        json_target.write('\t\t\"name\": \"' + hivent_name + '\",\n')
+
+        day, month, year = hivent_date.split(".")
+        json_target.write('\t\t\"day\": ' + day + ',\n')
+        json_target.write('\t\t\"month\": ' + month + ',\n')
+        json_target.write('\t\t\"year\": ' + year + ',\n')
+
+        json_target.write('\t\t\"long\": ' + hivent_long + ',\n')
+        json_target.write('\t\t\"lat\": ' + hivent_lat + ',\n')
+
+        json_target.write('\t\t\"content\": \"' + html_path + html_name + '\",\n')
+
+        json_target.write('\t}')
+        if row != rows[-1]:
+          json_target.write(',')
+        json_target.write('\n')
+
+  json_target.write("]")
+  json_target.close()
+
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
