ab90264fa03d799e9d583246e53609c6df9b7531
Felix Lauer <felix.lauer@uni-weimar.de>
2014-02-13 10:06:22 +0100

    fixed bug in Hivent loading, adapted release script to modules and changed colors for eu visualization

--- index.php
+++ index.php
@@ -225,7 +225,7 @@
       // }
 
       $(document).ready(function($) {
-        var histoglobe = new HG.HistoGlobe("config/fertility/modules.json");
+        var histoglobe = new HG.HistoGlobe("config/eu/modules.json");
       });
 
     </script>
--- make.sh
+++ make.sh
@@ -109,7 +109,7 @@ rosetta --jsOut "build/config.js" \
         --jsFormat "flat" \
         --jsTemplate $'var HGConfig;\n(function() {\n<%= preamble %>\nHGConfig = <%= blob %>;\n})();' \
         --cssOut "build/config.less" \
-        --cssFormat "less" config/fertility/style.rose
+        --cssFormat "less" config/eu/style.rose
 
 coffee -c -o build $cFiles
 
