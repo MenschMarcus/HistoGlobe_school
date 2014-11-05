0a70e11bfa8236e2cd98d008167e7aa39db16bba
Simon Schneegans <code@simonschneegans.de>
2014-11-05 15:33:18 +0100

    moved some files

--- make.sh
+++ make.sh
@@ -5,8 +5,8 @@
 # PROJECT=fertility
 # PROJECT=scandinavia
 # PROJECT=sdw
-PROJECT=teaser1_countries
-# PROJECT=teaser2_hivents
+# PROJECT=teaser1_countries
+PROJECT=teaser2_hivents
 # PROJECT=teaser3_sidebar
 
 (cd data_src/hivents/; ./generate.sh)
