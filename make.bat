92af81e37337dccccb8cfbc3ba6a35afd5afdf55
nnasuS <s-luge@t-online.de>
2014-11-06 11:08:50 +0100

    updated make.baz

--- make.bat
+++ make.bat
@@ -1,20 +1,16 @@
 @echo off
 rem set PROJECT=sdw
-rem set PROJECT=teaser1_countries
-set PROJECT=teaser2_hivents
+set PROJECT=teaser1_countries
+rem set PROJECT=teaser2_hivents
 rem set PROJECT=teaser3_sidebar
 
 call "C:\Program Files\nodejs\nodevars.bat"
 
 IF not exist build ( mkdir build )
 
-set cFiles=
-for /R script\ %%a in (*.coffee) do call set cFiles=%%cFiles%% %%a
-
-call coffee -c -o build %cFiles%
-
-set jFiles=
-for /R build\ %%a in (*.js) do call set jFiles=%%jFiles%% %%a
+for /R script\ %%a in (*.coffee) do (
+	call coffee -c -o build "%%a"
+)
 
 rosetta --jsOut "build/default_config.js" ^
         --jsFormat "flat" ^
@@ -26,6 +22,5 @@ rosetta --jsOut "build/config.js" ^
         --jsTemplate "(function() {<%%= preamble %%> $.extend(HGConfig, <%%= blob %%>);})();" ^
         --cssOut "build/config.less" ^
         --cssFormat "less" config/%PROJECT%/style.rose && ^
-uglifyjs %jFiles% -o script\histoglobe.min.js && ^
 lessc --no-color -x config\%PROJECT%\main.less style\histoglobe.min.css && ^
 call utils\replace_line.bat config.php 1 "<?php $config_path = '%PROJECT%'; ?>"
